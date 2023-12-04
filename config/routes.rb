11e01f5e7ae948a7124e2bffa4b8fc5f8ad965a5
Kevin Zolman <kevin.zolman@gmail.com>
2023-12-04 15:43:54 -0700

    Write in markets_request_spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      get '/markets', to: 'markets#index'
+      resources :markets, only: [:index, :show]
     end
   end
 end
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -8,6 +8,34 @@ describe "Markets API" do
 
     expect(response).to be_successful
 
-    markets = JSON.parse(response.body)
+    markets = JSON.parse(response.body, symbolize_names: true)
+
+    expect(markets.count).to eq(3)
+
+    markets.each do |market|
+      expect(market).to have_key(:id)
+      expect(market[:id]).to be_an(Integer)
+
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:zip)
+      expect(market[:zip]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+    end
   end
 end
