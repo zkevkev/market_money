79a01b0637b1f0932ae2a40ae28be17624003143
Kevin Zolman <kevin.zolman@gmail.com>
2023-12-04 16:42:59 -0700

    Write tests for single market endpoint

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "Markets API" do
-  it "sends a list of markets" do
+describe 'Markets API' do
+  it 'sends a list of markets' do
     create_list(:market, 3)
 
     get '/api/v0/markets'
@@ -40,5 +40,44 @@ describe "Markets API" do
       expect(market).to have_key(:vendor_count)
       expect(market[:vendor_count]).to be_a(Integer)
     end
+  end
+
+  it 'sends a single market' do
+    market = create(:market)
+
+    get "/api/v0/markets/#{market.id}"
+
+    expect(response).to be_successful
+
+    market = JSON.parse(response.body, symbolize_names: true)
+
+    expect(market).to be_a(Hash)
+
+    expect(market).to have_key(:id)
+    expect(market[:id]).to be_an(Integer)
+
+    expect(market).to have_key(:street)
+    expect(market[:street]).to be_a(String)
+
+    expect(market).to have_key(:city)
+    expect(market[:city]).to be_a(String)
+
+    expect(market).to have_key(:county)
+    expect(market[:county]).to be_a(String)
+
+    expect(market).to have_key(:state)
+    expect(market[:state]).to be_a(String)
+
+    expect(market).to have_key(:zip)
+    expect(market[:zip]).to be_a(String)
+
+    expect(market).to have_key(:lat)
+    expect(market[:lat]).to be_a(String)
+
+    expect(market).to have_key(:lon)
+    expect(market[:lon]).to be_a(String)
+
+    expect(market).to have_key(:vendor_count)
+    expect(market[:vendor_count]).to be_a(Integer)
   end
 end
