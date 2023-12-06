46be4039976b5b740fd49753125905024211738f
Kevin Zolman <kevin.zolman@gmail.com>
2023-12-05 19:02:55 -0700

    Implement market_vendor create invalid id sad path

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,4 +1,6 @@
 class Api::V0::MarketVendorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :not_found_response
+
   def create
     market_vendor = MarketVendor.create!(market_vendor_params)
     render json: MarketVendorSerializer.new(market_vendor), status: 201
@@ -8,5 +10,10 @@ class Api::V0::MarketVendorsController < ApplicationController
 
     def market_vendor_params
       params.require(:market_vendor).permit(:market_id, :vendor_id)
+    end
+
+    def not_found_response(exception)
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+        .serialize_json, status: :not_found
     end
 end
--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -19,4 +19,50 @@ describe 'MarketVendors API' do
       expect(created_market_vendor.vendor_id).to eq(market_vendor_params[:vendor_id])
     end
   end
+
+  context 'sad path' do
+    it "create will gracefully handle if a market id doesn't exist" do
+      vendor = create(:vendor)
+      market_vendor_params = ({
+                      market_id: 0,
+                      vendor_id: vendor.id
+                    })
+      headers = {'CONTENT_TYPE' => 'application/json'}
+    
+      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:detail]).to eq("Validation failed: Market must exist")
+    end
+
+    it "create will gracefully handle if a vendor id doesn't exist" do
+      market = create(:market)
+      market_vendor_params = ({
+                      market_id: market.id,
+                      vendor_id: 0
+                    })
+      headers = {'CONTENT_TYPE' => 'application/json'}
+    
+      post '/api/v0/market_vendors', headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:detail]).to eq("Validation failed: Vendor must exist")
+    end
+
+    it 'create will gracefully handle if a market_vendor already exists with given ids' do
+      
+    end
+  end
 end
